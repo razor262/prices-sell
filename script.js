c58310d4758daeddb13baaf1aa8ea3d9d4f8e78f
razor262 <24.work@bk.ru>
2022-12-20 15:53:09 +0300

    feat: add new comment

--- script.js
+++ script.js
@@ -6,11 +6,40 @@ if (a === 4) {
     console.log('work')
 }*/
 //fetch('http://api.openweathermap.org/data/2.5/weather?q=Krasnodar,ru&appid=4ca848bb82776e391cc1309aca8f5bfd')
+let i1 = document.querySelector('.i-1')
 
-fetch("https://evepraisal.com/a/corax.json")
+let b1 = document.querySelector('.b-1')
+b1.onclick = f1
+
+function f1() {
+  let currentSelect = i1.value
+  console.log(currentSelect)
+  i1.value = ''
+
+  fetch("https://evepraisal.com/item/venture.json")
   .then(function (resp) {
     return resp.json();
   })
   .then(function (data) {
     console.log(data);
+    let element
+    console.log(data.summaries)
+    let object = data.summaries
+    console.log(object)
+    for (const key in object) {
+        
+        element = object[key]
+
+        console.log(element)
+        console.log(element.market_display_name)
+    }
+   
+ 
+    /*
+    console.log(' Покупка ' + data.totals.buy)
+    console.log(' Продажа ' + data.totals.sell)
+    */
   });
+}
+
+
